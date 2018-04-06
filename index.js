bb1e08b257ae4c132bc67db8ccac0674ebc9162c
Tony Shindo <honolulu3310@gmail.com>
2018-04-05 21:17:36 -0400

    Add cluster mode

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration ) {}
-}
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but
+  // in child mode
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+} else {
+  // Im a child, im going to act like a server
+  // and do nothing else
+  const express = require('express');
+  const app = express();
 
-app.get('/', (req, res) => {
-  // The function below is going to be processed in event loop.
-  // This is blocking event loop.
-  doWork(5000);
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) { }
+  }
+
+  app.get('/', (req, res) => {
+    // The function below is going to be processed in event loop.
+    // This is blocking event looGp.
+    doWork(5000);
   
-  res.send('Hi there');
-});
+    res.send('Hi there');
+  });
 
-app.listen(3000);
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+  });
+
+  app.listen(3000);
+}
